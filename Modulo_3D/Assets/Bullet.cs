27ea52a1d84b0cf26d78c748806c07cc2f65c822
Jose Saldaña <josesaldanavi@gmail.com>
2018-08-02 15:57:42 -0500

    player

---
+++ Modulo_3D/Assets/Bullet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public float speed = 1;
+    public float lifespan = 3;
+    // Use this for initialization
+    void Start()
+    {
+        Destroy(gameObject, lifespan);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(transform.forward * speed * Time.deltaTime);
+    }
+}
---
+++ Modulo_3D/Assets/Movimiento.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour {
+
+    public float speed;
+    public float speedRotate;
+    public GameObject playerBullet;
+    public Transform gun;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(new Vector3(0, 35, 0)*speedRotate * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(new Vector3(0, -35, 0)*speedRotate * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Instantiate(playerBullet, gun.Find("canon").position, transform.rotation);
+        }
+	}
+}
